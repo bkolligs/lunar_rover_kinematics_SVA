29672d6cc20f6d2c32123c8870de96d7f5b41846
Ben Kolligs <benkolligs@gmail.com>
2022-01-20 10:17:00 -0500

    Added in a trajectory to test out the integration

--- prototyping/translation_estimation.py
+++ prototyping/translation_estimation.py
@@ -9,8 +9,8 @@ class Rover:
 	def __init__(self, initial_state: np.ndarray, timestep: float):
 		self.state = initial_state
 		self.delta_t = timestep
-		self.length = 0.1
-		self.width = 0.2
+		self.length = 0.5
+		self.width = 1
 
 	def dynamics(self, linear, angular):
 		x_dot = np.array(
@@ -25,14 +25,30 @@ class Rover:
 	def step(self, linear, angular):
 		self.state += self.dynamics(linear, angular) * self.delta_t
 	
-	def plot_simulation(self):
+	def plot_simulation(self, t=None):
 		plt.clf()
 		ax = plt.axes()
 		# plot the location and orientation
 		rectangle = plt.Rectangle(xy=(self.state[0], self.state[1]), width=self.width, height=self.length, angle=np.rad2deg(self.state[2]))
 		ax.add_patch(rectangle)
+		if t is not None: ax.set_title(f"Simulation at time {t:.2f}")
+		ax.set_xlim(-4, 4)
+		ax.set_ylim(-4, 4)
 		plt.draw()
 		plt.pause(0.01)
+
+def trajectory(time: float):
+	if time > 0 and time < 2:
+		return [0.2, 0]
+	
+	if time >= 2 and time < 4:
+		return [0.2, 0.2]
+	if time >= 4 and time < 8:
+		return [0.2, 0]
+	if time >= 8 and time < 10:
+		return [0.1, 0.1]
+	else: 
+		return [0, 0]
 
 
 if __name__ == '__main__':
@@ -40,8 +56,9 @@ if __name__ == '__main__':
 	times = np.arange(start, stop, dt)
 	rover = Rover([0, 0, 0], dt)
 	for t in times:
-		rover.step(0.1, 0.1)
-		rover.plot_simulation()
+		v, omega = trajectory(t)
+		rover.step(v, omega)
+		rover.plot_simulation(t=t)
 
 	print("Simulation Done")
 	plt.show()
